c220a4623a30963a9baaec15ba19c84bc40b7cb9
SLMCreatives <sulaiman@slmcreatives.com>
2025-01-27 01:20:52 +0800

    launch meta

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Landing Page",
+  title: "Launch 2025",
   description: `Golden Retreat Lombok Villas is a property development offering luxurious Villas set in the heart of Lombok’s best attractions.`,
   keywords: `golden retreat lombok villa, luxury villa, lombok villas, private pool, villas, island villas, island villa, private pools, poolside villa, pool villa, poolside villas, gold retreat lombok, gold retreat lombok villas, gold retreat lombok villa`
 };
